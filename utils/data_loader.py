56df2807237f269a452702fe7190458022dbdb52
Anthony Rinaldi <anthony.rinaldi04@gmail.com>
2024-04-11 19:09:02 +0000

    [wip] Black formatting data loader

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,7 +1,6 @@
 import json
-import os
-from pathlib import Path
 import traceback
+import typing
 
 import numpy as np
 import SimpleITK as sitk
@@ -11,14 +10,23 @@ from prefetch_generator import BackgroundGenerator
 from torch.utils.data import DataLoader, Dataset
 
 
-class DatasetMerged(Dataset): 
-    def __init__(self, paths, mode='train', data_type='Tr', image_size=128, 
-                 transform=None, threshold=10,
-                 split_num=1, split_idx=0, pcc=False):
+class DatasetMerged(Dataset):
+    def __init__(
+        self,
+        paths,
+        mode="train",
+        data_type="Tr",
+        image_size=128,
+        transform=None,
+        threshold=10,
+        split_num=1,
+        split_idx=0,
+        pcc=False,
+    ):
         self.paths = paths
         self.data_type = data_type
-        self.split_num=split_num
-        self.split_idx=split_idx
+        self.split_num = split_num
+        self.split_idx = split_idx
 
         self._set_file_paths(self.paths)
         self.image_size = image_size
@@ -26,7 +34,7 @@ class DatasetMerged(Dataset):
         self.threshold = threshold
         self.mode = mode
         self.pcc = pcc
-    
+
     def __len__(self):
         return len(self.label_paths)
 
@@ -41,8 +49,8 @@ class DatasetMerged(Dataset):
             sitk_image.SetDirection(sitk_label.GetDirection())
 
         subject = tio.Subject(
-            image = tio.ScalarImage.from_sitk(sitk_image),
-            label = tio.LabelMap.from_sitk(sitk_label),
+            image=tio.ScalarImage.from_sitk(sitk_image),
+            label=tio.LabelMap.from_sitk(sitk_label),
         )
 
         # if '/ct_' in self.image_paths[index]: # TODO: what is this for?
@@ -55,27 +63,39 @@ class DatasetMerged(Dataset):
                 print(traceback.format_exc())
                 print(self.image_paths[index])
 
-        if(self.pcc):
+        if self.pcc:
             print("using pcc setting")
             # crop from random click point
             random_index = torch.argwhere(subject.label.data == 1)
-            if(len(random_index)>=1):
+            if len(random_index) >= 1:
                 random_index = random_index[np.random.randint(0, len(random_index))]
                 # print(random_index)
                 crop_mask = torch.zeros_like(subject.label.data)
                 # print(crop_mask.shape)
-                crop_mask[random_index[0]][random_index[1]][random_index[2]][random_index[3]] = 1
-                subject.add_image(tio.LabelMap(tensor=crop_mask,
-                                                affine=subject.label.affine),
-                                    image_name="crop_mask")
-                subject = tio.CropOrPad(mask_name='crop_mask', 
-                                        target_shape=(self.image_size,self.image_size,self.image_size))(subject)
-        
-        if self.mode == "train" and self.data_type == 'Tr':
-            return subject.image.data.clone().detach(), subject.label.data.clone().detach()
+                crop_mask[random_index[0]][random_index[1]][random_index[2]][
+                    random_index[3]
+                ] = 1
+                subject.add_image(
+                    tio.LabelMap(tensor=crop_mask, affine=subject.label.affine),
+                    image_name="crop_mask",
+                )
+                subject = tio.CropOrPad(
+                    mask_name="crop_mask",
+                    target_shape=(self.image_size, self.image_size, self.image_size),
+                )(subject)
+
+        if self.mode == "train" and self.data_type == "Tr":
+            return (
+                subject.image.data.clone().detach(),
+                subject.label.data.clone().detach(),
+            )
         else:
-            return subject.image.data.clone().detach(), subject.label.data.clone().detach(), self.image_paths[index]
- 
+            return (
+                subject.image.data.clone().detach(),
+                subject.label.data.clone().detach(),
+                self.image_paths[index],
+            )
+
     def _set_file_paths(self, paths):
         self.image_paths = []
         self.label_paths = []
@@ -84,33 +104,40 @@ class DatasetMerged(Dataset):
 
         # if ${path}/labelsTr exists, search all .nii.gz
         for path in paths:
-            with open(path, 'r') as f:
+            with open(path, "r") as f:
                 json_data = json.load(f)
 
-            self.image_paths.extend([x['image'] for x in json_data if x['volume'] > self.threshold])
-            self.label_paths.extend([x['label'] for x in json_data if x['volume'] > self.threshold])
-            self.label_volumes.extend([x['volume'] for x in json_data if x['volume'] > self.threshold])
-            self.image_spacing.extend([x['spacing'] for x in json_data if x['volume'] > self.threshold])
+            self.image_paths.extend(
+                [x["image"] for x in json_data if x["volume"] > self.threshold]
+            )
+            self.label_paths.extend(
+                [x["label"] for x in json_data if x["volume"] > self.threshold]
+            )
+            self.label_volumes.extend(
+                [x["volume"] for x in json_data if x["volume"] > self.threshold]
+            )
+            self.image_spacing.extend(
+                [x["spacing"] for x in json_data if x["volume"] > self.threshold]
+            )
 
     def get_filtered_json(self) -> typing.Dict:
         return [
             {
-                'image': self.image_paths[i],
-                'label': self.label_paths[i],
-                'volume': self.label_volumes[i],
-                'spacing': self.image_spacing[i]
+                "image": self.image_paths[i],
+                "label": self.label_paths[i],
+                "volume": self.label_volumes[i],
+                "spacing": self.image_spacing[i],
             }
             for i in range(len(self.image_paths))
         ]
 
+
 class DatasetValidation(DatasetMerged):
     def _set_file_paths(self, paths):
         super()._set_file_paths(paths)
-        
-        self.image_paths = self.image_paths[self.split_idx::self.split_num]
-        self.label_paths = self.label_paths[self.split_idx::self.split_num]
 
-
+        self.image_paths = self.image_paths[self.split_idx :: self.split_num]
+        self.label_paths = self.label_paths[self.split_idx :: self.split_num]
 
 
 class BackgroundDataLoader(DataLoader):
@@ -118,7 +145,7 @@ class BackgroundDataLoader(DataLoader):
         return BackgroundGenerator(super().__iter__())
 
 
-class Test_Single(Dataset): 
+class Test_Single(Dataset):
     def __init__(self, paths, image_size=128, transform=None, threshold=500):
         self.paths = paths
 
@@ -126,7 +153,7 @@ class Test_Single(Dataset):
         self.image_size = image_size
         self.transform = transform
         self.threshold = threshold
-    
+
     def __len__(self):
         return len(self.label_paths)
 
@@ -141,12 +168,12 @@ class Test_Single(Dataset):
             sitk_image.SetDirection(sitk_label.GetDirection())
 
         subject = tio.Subject(
-            image = tio.ScalarImage.from_sitk(sitk_image),
-            label = tio.LabelMap.from_sitk(sitk_label),
+            image=tio.ScalarImage.from_sitk(sitk_image),
+            label=tio.LabelMap.from_sitk(sitk_label),
         )
 
-        if '/ct_' in self.image_paths[index]:
-            subject = tio.Clamp(-1000,1000)(subject)
+        if "/ct_" in self.image_paths[index]:
+            subject = tio.Clamp(-1000, 1000)(subject)
 
         if self.transform:
             try:
@@ -154,16 +181,18 @@ class Test_Single(Dataset):
             except:
                 print(self.image_paths[index])
 
-
         if subject.label.data.sum() <= self.threshold:
             return self.__getitem__(np.random.randint(self.__len__()))
-        
 
-        return subject.image.data.clone().detach(), subject.label.data.clone().detach(), self.image_paths[index]
-    
+        return (
+            subject.image.data.clone().detach(),
+            subject.label.data.clone().detach(),
+            self.image_paths[index],
+        )
+
     def _set_file_paths(self, paths):
         self.image_paths = []
         self.label_paths = []
 
         self.image_paths.append(paths)
-        self.label_paths.append(paths.replace('images', 'labels'))
+        self.label_paths.append(paths.replace("images", "labels"))
