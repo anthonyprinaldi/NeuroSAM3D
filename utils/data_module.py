cf99fad1ca8aecd3ea2dd0181885d0d0992df303
Anthony Rinaldi <anthony.rinaldi04@gmail.com>
2024-08-06 11:03:01 -0400

    [feat] Add transforms to a separate file

--- utils/data_module.py
+++ utils/data_module.py
@@ -6,7 +6,7 @@ import monai.data
 import monai.transforms
 from torch.utils.data import DataLoader
 
-from .data_loader import BackgroundDataLoader, DatasetMerged
+from .transforms import get_train_trainsforms, get_val_transforms
 
 
 class NeuroSAMDataModule(L.LightningDataModule):
@@ -14,85 +14,8 @@ class NeuroSAMDataModule(L.LightningDataModule):
     def __init__(self, args) -> None:
         super().__init__()
 
-        self.train_transforms = monai.transforms.Compose(
-            [
-                monai.transforms.LoadImaged(keys=["image", "label"], ensure_channel_first=True),
-                monai.transforms.Orientationd(keys=["image", "label"], axcodes="RAS"),
-                monai.transforms.ScaleIntensityRangePercentilesd(
-                    keys=["image"],
-                    lower=0.05,
-                    upper=99.95,
-                    b_min=-1,
-                    b_max=1,
-                    clip=True
-                ),
-                # monai.transforms.CropForegroundd( # TODO: do we need?
-                #     keys=["image", "label"],
-                #     source_key="image",
-                #     select_fn=lambda x: x > -1,
-                # ),
-                monai.transforms.Compose([
-                    monai.transforms.RandFlipd(keys=["image", "label"], prob=0.3, spatial_axis=0),
-                    monai.transforms.RandFlipd(keys=["image", "label"], prob=0.3, spatial_axis=1),
-                    monai.transforms.RandFlipd(keys=["image", "label"], prob=0.3, spatial_axis=2),
-                    monai.transforms.RandRotate90d(keys=["image", "label"], prob=0.3, spatial_axes=(0, 1)),
-                    monai.transforms.RandRotate90d(keys=["image", "label"], prob=0.3, spatial_axes=(1, 2)),
-                    monai.transforms.RandRotate90d(keys=["image", "label"], prob=0.3, spatial_axes=(0, 2)),
-                ]),
-                monai.transforms.ToTensord(keys=["image", "label"]),
-                monai.transforms.OneOf([
-                    monai.transforms.RandAdjustContrastd(keys=["image"], prob=0.5, gamma=(0.5, 2.0)),
-                    monai.transforms.RandGaussianNoised(keys=["image"], prob=0.5),
-                    monai.transforms.RandGaussianSmoothd(keys=["image"], prob=0.5),
-                    monai.transforms.RandHistogramShiftd(keys=["image"], prob=0.5, num_control_points=10),
-                    monai.transforms.RandGaussianSharpend(keys=["image"], prob=0.5),
-                ]),
-                monai.transforms.RandCropByPosNegLabeld(
-                    keys=["image", "label"],
-                    label_key="label",
-                    spatial_size=[args.img_size, args.img_size, args.img_size],
-                    pos=1,
-                    neg=0,
-                    num_samples=1,
-                    allow_smaller=True,
-                ),
-                # ensure that we have the right shape in the end
-                monai.transforms.ResizeWithPadOrCropd(
-                    keys=["image", "label"],
-                    spatial_size=[args.img_size, args.img_size, args.img_size],
-                ),
-                monai.transforms.EnsureTyped(keys=["image", "label"]),
-            ]
-        )
-
-        self.val_transforms = monai.transforms.Compose(
-            [
-                monai.transforms.LoadImaged(keys=["image", "label"], ensure_channel_first=True),
-                monai.transforms.Orientationd(keys=["image", "label"], axcodes="RAS"),
-                monai.transforms.ScaleIntensityRangePercentilesd(
-                    keys=["image"],
-                    lower=0.05,
-                    upper=99.95,
-                    b_min=-1,
-                    b_max=1,
-                    clip=True
-                ),
-                # monai.transforms.CropForegroundd( # TODO: do we need?
-                #     keys=["image", "label"],
-                #     source_key="image",
-                #     select_fn=lambda x: x > -1,
-                # ),
-                monai.transforms.ToTensord(keys=["image", "label"]),
-                # TODO: do we need this?
-                # monai.transforms.ResizeWithPadOrCropd(
-                #     keys=["image", "label"],
-                #     spatial_size=[args.img_size, args.img_size, args.img_size],
-                # ),
-                monai.transforms.EnsureTyped(keys=["image", "label"]),
-            ]
-        )
-
-        self.args = args
+        self.train_transforms = get_train_trainsforms(img_size)
+        self.val_transforms = get_val_transforms(img_size)
 
     
     def prepare_data(self) -> None:
