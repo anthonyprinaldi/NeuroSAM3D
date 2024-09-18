803ee90e43c1f82f2aecbc38f0ba68d069608870
Anthony Rinaldi <anthony.rinaldi04@gmail.com>
2024-09-18 13:55:06 -0400

    [fix] Update training script

--- train.py
+++ train.py
@@ -1,13 +1,18 @@
+import lightning as L
+import torch
+
 from segment_anything import NeuroSamModel
 from utils.cli_utils import LoggerSaveConfigCallback, NeuroSAMCLI
 from utils.data_module import NeuroSamDataModule
+
+torch.set_float32_matmul_precision('medium')
 
 
 def cli_main():
     cli = NeuroSAMCLI(
         NeuroSamModel,
         NeuroSamDataModule,
-        seed_everything_default=0,
+        seed_everything_default=2024,
         save_config_callback=LoggerSaveConfigCallback,
         save_config_kwargs={
             "save_to_log_dir": True,
